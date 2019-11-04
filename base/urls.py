750f526d88cfd8545aaf8eda4a7826cdec81f305
Keiichiro Yamamoto <k.yanmar@gmail.com>
2019-11-04 23:14:44 +0900

    Display Hello world

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 name = 'base'
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.top, name='top'),
+]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
 
-# Create your views here.
+def top(request):
+  return HttpResponse('Hello world!')
